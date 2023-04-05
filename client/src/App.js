e5ff0c423f9c631acb66836e614aa8776c262db8
Prashant Puri <ppuri7069@gmail.com>
2023-04-06 00:53:02 +0545

    Create Routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+// Impprt all components
+import PageNotFound from './components/PageNotFound';
+import Password from './components/Password';
+import Profile from './components/Profile';
+import Recovery from './components/Recovery';
+import Register from './components/Register';
+import Reset from './components/Reset';
+import Username from './components/Username';
+
+
+
+
+/* Root routes */
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Username>Root Route</Username>
+    },
+    {
+        path: '/register',
+        element: <Register>Register Route</Register>
+    },
+    {
+        path: '/reset',
+        element: <Reset>Login Route</Reset>
+    },
+    {
+        path: '/recovery',
+        element: <Recovery>Home Route</Recovery>
+    },
+    {
+        path: '/profile',
+        element: <Profile>Profile Route</Profile>
+    },
+    {
+        path: '*', // 404 Invalid Page
+        element: <PageNotFound>Page Not Found</PageNotFound>
+    },
+    {
+        path: '/Password',
+        element: <Password>Password Route</Password>
+    },
+
+])
+
+export default function App() {
+    return (
+        <main>
+            <RouterProvider router={router}></RouterProvider>
+        </main >
+    )
 }
-
-export default App;
