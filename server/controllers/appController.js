2ffc76e2fe1a940f69a9072a9a312a2897708f26
Prashant Puri <ppuri7069@gmail.com>
2023-04-16 03:20:26 +0545

    fixed register error {500 Internal error}

--- server/controllers/appController.js
+++ server/controllers/appController.js
@@ -1,5 +1,23 @@
 import UserModel from '../model/User.model.js';
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import ENV from '../config.js';
+
+/** middleware for verify user */
+export async function verifyUser(req, res, next) {
+    try {
+
+        const { username } = req.method == "GET" ? req.query : req.body;
+
+        // check the user existance
+        let exist = await UserModel.findOne({ username });
+        if (!exist) return res.status(404).send({ error: "Can't find User!" });
+        next();
+
+    } catch (error) {
+        return res.status(404).send({ error: "Authentication Error" });
+    }
+}
 
 
 /** POST: http://localhost:8080/api/register 
@@ -19,60 +37,40 @@ export async function register(req, res) {
     try {
         const { username, password, profile, email } = req.body;
 
-        // check the existing user
-        const existUsername = new Promise((resolve, reject) => {
-            UserModel.findOne({ username }, function (err, user) {
-                if (err) reject(new Error(err))
-                if (user) reject({ error: "Please use unique username" });
+        // Check for existing username
+        const existUsername = await UserModel.findOne({ username }).exec();
+        if (existUsername) {
+            return res.status(400).send({ error: "Please use a unique username" });
+        }
 
-                resolve();
-            })
+        // Check for existing email
+        const existEmail = await UserModel.findOne({ email }).exec();
+        if (existEmail) {
+            return res.status(400).send({ error: "Please use a unique email" });
+        }
+
+        // Hash the password
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        // Create a new user instance
+        const user = new UserModel({
+            username,
+            password: hashedPassword,
+            profile: profile || '',
+            email
         });
 
-        // check for existing email
-        const existEmail = new Promise((resolve, reject) => {
-            UserModel.findOne({ email }, function (err, email) {
-                if (err) reject(new Error(err))
-                if (email) reject({ error: "Please use unique Email" });
+        // Save the user to the database
+        await user.save();
 
-                resolve();
-            })
-        });
-
-
-        Promise.all([existUsername, existEmail])
-            .then(() => {
-                if (password) {
-                    bcrypt.hash(password, 10)
-                        .then(hashedPassword => {
-
-                            const user = new UserModel({
-                                username,
-                                password: hashedPassword,
-                                profile: profile || '',
-                                email
-                            });
-
-                            // return save result as a response
-                            user.save()
-                                .then(result => res.status(201).send({ msg: "User Register Successfully" }))
-                                .catch(error => res.status(500).send({ error }))
-
-                        }).catch(error => {
-                            return res.status(500).send({
-                                error: "Enable to hashed password"
-                            })
-                        })
-                }
-            }).catch(error => {
-                return res.status(500).send({ error })
-            })
-
-
+        // Return success response
+        res.status(201).send({ msg: "User registered successfully" });
     } catch (error) {
-        return res.status(500).send(error);
+        console.error(error);
+        res.status(500).send({ error: "An error occurred during registration" });
     }
 }
+
 
 
 /** POST: http://localhost:8080/api/login 
@@ -82,7 +80,41 @@ export async function register(req, res) {
 }
 */
 export async function login(req, res) {
-    res.json('login route');
+    const { username, password } = req.body;
+
+    try {
+
+        UserModel.findOne({ username })
+            .then(user => {
+                bcrypt.compare(password, user.password)
+                    .then(passwordCheck => {
+
+                        if (!passwordCheck) return res.status(400).send({ error: "Don't have Password" });
+
+                        // create jwt token
+                        const token = jwt.sign({
+                            userId: user._id,
+                            username: user.username
+                        }, ENV.JWT_SECRET, { expiresIn: "24h" });
+
+                        return res.status(200).send({
+                            msg: "Login Successful...!",
+                            username: user.username,
+                            token
+                        });
+
+                    })
+                    .catch(error => {
+                        return res.status(400).send({ error: "Password does not Match" })
+                    })
+            })
+            .catch(error => {
+                return res.status(404).send({ error: "Username not Found" });
+            })
+
+    } catch (error) {
+        return res.status(500).send({ error });
+    }
 }
 
 
