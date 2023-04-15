5072bc229d0cb0e719b7b0541a10111246953a93
Prashant Puri <ppuri7069@gmail.com>
2023-04-16 05:23:54 +0545

    generated OTP through opt-generator

--- server/controllers/appController.js
+++ server/controllers/appController.js
@@ -2,6 +2,7 @@ import UserModel from '../model/User.model.js';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import ENV from '../config.js';
+import otpGenerator from 'otp-generator';
 
 /** middleware for verify user */
 export async function verifyUser(req, res, next) {
@@ -186,19 +187,29 @@ export async function updateUser(req, res) {
 
 /** GET: http://localhost:8080/api/generateOTP */
 export async function generateOTP(req, res) {
-    res.join('generateOTP route');
+    req.app.locals.OTP = await otpGenerator.generate(6, { lowerCaseAlphabets: false, upperCaseAlphabets: false, specialChars: false })
+    res.status(201).send({ code: req.app.locals.OTP })
 }
 
 /** GET: http://localhost:8080/api/verifyOTP */
 export async function verifyOTP(req, res) {
-    res.join('generateOTP route');
+    const { code } = req.query;
+    if (parseInt(req.app.locals.OTP) === parseInt(code)) {
+        req.app.locals.OTP = null; // reset the OTP value
+        req.app.locals.resetSession = true; // start session for reset password
+        return res.status(201).send({ msg: 'Verify Successsfully!' })
+    }
+    return res.status(400).send({ error: "Invalid OTP" });
 }
 
 
 // successfully redirect user when OTP is valid
 /** GET: http://localhost:8080/api/createResetSession */
 export async function createResetSession(req, res) {
-    res.join('verifyOTP route');
+    if (req.app.locals.resetSession) {
+        return res.status(201).send({ msg: "access granted!" })
+    }
+    return res.status(440).send({ error: "Session expired!" })
 }
 
 // update the password when we have valid session
