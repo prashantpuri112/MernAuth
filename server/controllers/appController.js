d34aeb59edd82dff849d893217e640496680664a
Prashant Puri <ppuri7069@gmail.com>
2023-04-16 05:38:55 +0545

    Added resetpassword functionality with otp verification

--- server/controllers/appController.js
+++ server/controllers/appController.js
@@ -215,5 +215,40 @@ export async function createResetSession(req, res) {
 // update the password when we have valid session
 /** PUT: http://localhost:8080/api/resetPassword */
 export async function resetPassword(req, res) {
-    res.join('resetPassword route');
+    try {
+
+        if (!req.app.locals.resetSession) return res.status(440).send({ error: "Session expired!" });
+
+        const { username, password } = req.body;
+
+        try {
+
+            UserModel.findOne({ username })
+                .then(user => {
+                    bcrypt.hash(password, 10)
+                        .then(hashedPassword => {
+                            UserModel.updateOne({ username: user.username },
+                                { password: hashedPassword }, function (err, data) {
+                                    if (err) throw err;
+                                    req.app.locals.resetSession = false; // reset session
+                                    return res.status(201).send({ msg: "Record Updated...!" })
+                                });
+                        })
+                        .catch(e => {
+                            return res.status(500).send({
+                                error: "Enable to hashed password"
+                            })
+                        })
+                })
+                .catch(error => {
+                    return res.status(404).send({ error: "Username not Found" });
+                })
+
+        } catch (error) {
+            return res.status(500).send({ error })
+        }
+
+    } catch (error) {
+        return res.status(401).send({ error })
+    }
 }
