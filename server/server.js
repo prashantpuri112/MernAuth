ccd56af70dbb48967a86e286aef8cf81377f5920
Prashant Puri <ppuri7069@gmail.com>
2023-04-16 01:52:53 +0545

    created controller endpoint

--- server/server.js
+++ server/server.js
@@ -2,22 +2,28 @@ import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
 import connect from './database/conn.js';
+import router from './router/route.js';
 
-
+// Create an instance of the express app
 const app = express();
 
-// Middleware
+/** middlewares */
 app.use(express.json());
 app.use(cors());
 app.use(morgan('tiny'));
-app.disable('x-powered-by'); // Hide the powered by express
+app.disable('x-powered-by'); // less hackers know about our stack
+
 
 const port = 8080;
 
-// HTTP GET REQUEST
+/** HTTP GET Request */
 app.get('/', (req, res) => {
     res.status(201).json("Home GET Request");
 });
+
+
+/** api routes */
+app.use('/api', router)
 
 /** start server only when we have valid connection */
 connect().then(() => {
