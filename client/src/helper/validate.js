9deaeaa544df066d15f6f123e52e6af5f8fbaadc
Prashant Puri <ppuri7069@gmail.com>
2023-04-16 00:19:07 +0545

    Register Component done

--- client/src/helper/validate.js
+++ client/src/helper/validate.js
@@ -36,6 +36,17 @@ export async function resetPasswordValidation(values) {
     return errors;
 }
 
+
+/** validate register form */
+export async function registerValidation(values) {
+    const errors = usernameVerify({}, values);
+    passwordVerify(errors, values);
+    emailVerify(errors, values);
+
+    return errors;
+}
+
+
 /** validate password */
 function passwordVerify(errors = {}, values) {
     /* eslint-disable no-useless-escape */
@@ -63,4 +74,18 @@ function usernameVerify(error = {}, values) {
         error.username = toast.error('Invalid Username...!');
     }
     return error
+}
+
+
+/** validate email */
+function emailVerify(error = {}, values) {
+    if (!values.email) {
+        error.email = toast.error("Email Required...!");
+    } else if (values.email.includes(" ")) {
+        error.email = toast.error("Wrong Email...!")
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+        error.email = toast.error("Invalid email address...!")
+    }
+
+    return error;
 }
