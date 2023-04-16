f4ba3f263b4aba9178d84602e7f7480176a98b35
Prashant Puri <ppuri7069@gmail.com>
2023-04-16 08:12:42 +0545

    changed initialValue to empty

--- server/database/conn.js
+++ server/database/conn.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import { MongoMemoryServer } from "mongodb-memory-server";
+import ENV from "../config.js";
 
 async function connect() {
     const mongod = await MongoMemoryServer.create();
@@ -7,7 +8,8 @@ async function connect() {
 
     mongoose.set('strictQuery', true)
 
-    const db = await mongoose.connect(getUri);
+    // const db = await mongoose.connect(getUri);
+    const db = await mongoose.connect(ENV.ATLAS_URI);
     console.log("Database connected");
     return db;
 }
